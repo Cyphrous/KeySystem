668308d2533ebe80851f3c01972112362786a95f
Cyph <96255759+Cyphrous@users.noreply.github.com>
2021-12-20 17:46:06 +0800

    Update KeySystem

--- Main/KeySystem.lua
+++ Main/KeySystem.lua
@@ -1,52 +1,27 @@
-local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/Cyphrous/Moonless-Ui-Library/main/Main/Source.lua")()
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/Cyphrous/KeySystem/main/Main/MainSource.lua")()
 
-local win = Flux:Window("Moonless Hub", "Key System | MlH v0.1", Color3.fromRGB(212, 57, 165))
-local tab = win:Tab("Key System", "http://www.roblox.com/asset/?id=54479722")
-    tab:Line()
-    tab:Label("                                                      Enter Key Here:")
-    tab:Line()
-    tab:Textbox("Key", "Dont have the key or you have the wrong one ? Press Get Key!", true, function(t)
-            local key = t
-            local url = "https://moonlesshub.000webhostapp.com/index.php?key="..key
-            if game:HttpGet(url) == key then
-            wait(1)
-            if game.PlaceId == ID then
-                -- script to run
-               end
-            local Part = game:GetService("CoreGui")["Moonless Hub"]
-            Part:Destroy()
-            else
-                Flux:Notification("Invalid Key!\n Please Enter a Valid key.", "Okay")
-            wait(1)
-            end
-        end) 
-        tab:Button("                                           GET KEY!(CLICK ME)", "                       Copy Link of the Discord to Get the Key Link", function()
-            Flux:Notification("Copy Successful !", "Alright")
-            setclipboard("https://discord.gg/x3wtp3A3S5")
-            end)
-        tab:Line()
-        
---[[
-tab:Button("Kill all", "This function may not work sometimes and you can get banned.", function()
-Flux:Notification("Killed all players successfully!", "Alright")
-end)
-tab:Label("This is just a label.", "Description ?")
-tab:Line()
-tab:Toggle("Auto-Farm Coins", "Automatically collects coins for you!", function(t)
-print(t)
-end)
-tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
-print(t)
-end)
-tab:Dropdown("Part to aim at", {"Torso","Head","Penis"}, function(t)
-print(t)
-end)
-tab:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
-end)
-tab:Textbox("Gun Power", "This textbox changes your gun power, so you can kill everyone faster and easier.", true, function(t)
-print(t)
-end)
-tab:Bind("Kill Bind", Enum.KeyCode.Q, function()
-print("Killed a random person!")
-end)]]--
+local win = lib:Window("MOONLESS",Color3.fromRGB(255, 0, 191), Enum.KeyCode.RightControl)
+
+local tab = win:Tab("KEYSYSTEM")
+
+tab:Label("Enter Key Here:")
+tab:Textbox("Key", "Dont have the key or you have the wrong one ? Press Get Key!", true, function(t)
+    local key = t
+    local url = "http://moonless.site/auth/?key="..key
+    if game:HttpGet(url) == key then
+    wait(1)
+    if game.PlaceId == ID then
+        -- script to run
+       end
+       print("Whitelisted")
+    local Part = game:GetService("CoreGui")["Moonless UI"]
+    Part:Destroy()
+    else
+        lib:Notification("Wrong Key","Invalid Key!\n Please Enter a Valid key.","Okay")
+    wait(1)
+    end
+end) 
+    tab:Button("GET KEY!(CLICK ME)", function()
+        lib:Notification("You Copied The\n Discord Link","Copy Successful !", "Alright")
+        setclipboard("https://discord.gg/x3wtp3A3S5")
+        end)
